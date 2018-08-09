b3652094a602c10a0a30d6036971eb48f626850c
황진수-PC <cjswp122@gmail.com>
2018-08-09 10:45:57 +0900

    redux thunk use async await

--- src/reducers/post.ts
+++ src/reducers/post.ts
@@ -9,26 +9,26 @@ const GET_POST_PENDING = 'GET_POST_PENDING';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_FAILURE = 'GET_POST_FAILURE';
 
-export const getPost = (postId: number) => (dispatch: Dispatch): Promise<any> => {
-  // 먼저, 요청이 시작했다는것을 알립니다
-  dispatch({type: GET_POST_PENDING});
+const getPostSuccess = response => ({
+  type: GET_POST_SUCCESS,
+  payload: response,
+});
 
-  // 요청을 시작합니다
-  // 여기서 만든 promise 를 return 해줘야, 나중에 컴포넌트에서 호출 할 때 getPost().then(...) 을 할 수 있습니다
-  return getPostAPI(postId).then(
-    (response) => {
-      // 요청이 성공했을경우, 서버 응답내용을 payload 로 설정하여 GET_POST_SUCCESS 액션을 디스패치합니다.
-      dispatch({
-          type: GET_POST_SUCCESS,
-          payload: response,
-      });
-    },
-  ).catch ((error) => {
-    dispatch({
-      type: GET_POST_FAILURE,
-      payload: error,
-    });
-  });
+const getPostFailure = error => ({
+  type: GET_POST_FAILURE,
+  payload: error,
+});
+
+const getPost = (postId: number) => async (dispatch: Dispatch) => {
+  dispatch({type: GET_POST_PENDING});
+  try {
+    const response = await getPostAPI(postId);
+    if (response) {
+      dispatch(getPostSuccess(response));
+    }
+  } catch (error) {
+    dispatch(getPostFailure(error));
+  }
 };
 
 export const actionCreators = {
